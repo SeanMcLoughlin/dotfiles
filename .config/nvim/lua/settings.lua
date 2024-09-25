ed3051fbe284fb5716915557fd0d1b8f1139d5f7
Sean McLoughlin <smclough@protonmail.com>
2024-09-25 11:41:19 -0400

    Re-add basic nvim config

---
+++ .config/nvim/lua/settings.lua
@@ -0,0 +1,3 @@
+vim.opt.incsearch = true  -- Show search matches as you type
+vim.opt.ignorecase = true -- Make searches case-insensitive by default
+vim.opt.smartcase = true  -- Enable SmartSearch, making the search case-sensitive if it contains uppercase letters
