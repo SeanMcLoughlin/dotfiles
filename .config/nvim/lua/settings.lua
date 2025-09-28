02b84a676c8cbf28cf1ae7851afd658504a390a4
Sean McLoughlin <smclough@protonmail.com>
2025-09-28 19:17:18 -0400

    Add new neovim setup

--- .config/nvim/lua/settings.lua
+++
@@ -1,3 +0,0 @@
-vim.opt.incsearch = true  -- Show search matches as you type
-vim.opt.ignorecase = true -- Make searches case-insensitive by default
-vim.opt.smartcase = true  -- Enable SmartSearch, making the search case-sensitive if it contains uppercase letters
